33ac3a87dd3e0d9c03c4bf90a211e01a3cb596fe
æLtorio <aeltorio@universidadatlantico.org>
2025-02-13 19:02:50 +0100

    move computations in a separate file for reusing

--- src/aga8-sonic-nozzle-vue/src/cards/Compute.vue
+++ src/aga8-sonic-nozzle-vue/src/cards/Compute.vue
@@ -20,8 +20,6 @@
 import AGA8wasm, {
   type MainModule,
   type GasMixture,
-  type PropertiesDetailResult,
-  type PropertiesGERGResult,
 } from "@sctg/aga8-js";
 import { initFlowbite } from "flowbite";
 import { Chart } from "chart.js/auto";
@@ -31,7 +29,8 @@ import { ScientificNotation } from "../utilities/scientific";
 import Temml from "temml";
 import DoubleRange from "../components/DoubleRange.vue";
 import _nistGasMixture from "../../../examples/NG_Compositions.json" with { type: "json" };
-import { Polyfit, type NumberArray } from "@sctg/polyfitjs";
+import { type NumberArray } from "@sctg/polyfitjs";
+import { getThoroidalNozzleDischargeCoefficient, getMassFlowRateDataset, type Method, type MassFlowRate, getPolyfitFlow } from "../utilities/sonic";
 
 type GasMixtureExt = {
   name: string;
@@ -191,19 +190,6 @@ const availableGasMixtures = [
   ...nistGasMixture,
 ] as AvailableGasMixtures;
 
-type Method = "DETAIL" | "GERG-2008";
-type MassFlowRate = {
-  massFlowRate: number; // mass flow rate in kg/s
-  volumeFlowRateAtOutputPressure: number; // volume flow rate in m³/s
-  volumeFlowRateAt1atm: number; // volume flow rate at 1 atm in m³/s
-  temperature: number; // temperature in K
-  pressure: number; // pressure in kPa
-  specificNozzleCoefficient: number; // specific nozzle coefficient
-  crticalPresure: number; // critical pressure in kPa
-  M: number; // Molar mass
-  kappa: number; // Isentropic exponent
-  Cf: number; //	Critical Flow coefficient
-};
 
 const method = ref<Method>("DETAIL");
 const menuOpen = ref(false);
@@ -222,7 +208,6 @@ const linearPolynomials = useTemplateRef<HTMLDivElement>("linearPolynomials");
 const showPrecision = ref(false);
 let chartKgS: Chart | null = null;
 let chartLs: Chart | null = null;
-const R = 8.31446261815324; // Universal gas constant in J/(mol·K)
 const nbGraphSteps = 1000;  // Number of steps for the graph
 const correlation = ref(1 - 1e-6);
 type AvailableGasMixtures = GasMixtureExt[];
@@ -259,7 +244,7 @@ const Pout = ref(250); // kPa
 
 const totalPercent = ref(100); // Total percentage
 
-let AGA8: MainModule | null = null;
+let AGA8: MainModule | undefined = undefined;
 
 /**
  * Loads the AGA8 WASM module
@@ -282,172 +267,6 @@ onMounted(() => {
   });
 });
 
-/**
- * Compute the discharge coefficient for a toroidal nozzle given the Reynolds number
- * cf: https://www.methyinfra.ptb.de/fileadmin/documents/empir-2021/methyinfra/documents/MetHyInfra_Nozzle_WS_MacDonald_Introduction_to_Sonic_Nozzles_and_ISO_9300.pdf p16
- * @param Re_thoroidal - Reynolds number for thoroidal nozzle
- */
-function getThoroidalNozzleDischargeCoefficient(Re_thoroidal: number): number {
-  const Cd_a = 0.9959; // Constant for toroidal nozzle
-  const Cd_b = 2.72; // Reynolds number factor for toroidal nozzle
-  const Cd_n = 0.5; // Reynolds number exponent for toroidal nozzle
-  const Cd = Cd_a - Cd_b * Re_thoroidal ** (Cd_n * -1);
-  return Cd;
-}
-
-/**
- * Compute the maximal outlet pressure for a given inlet pressure and critical flow
- * @param inletPressure
- * @param criticalFlow
- */
-function getMaximalOutletPressure(
-  inletPressure: number,
-  criticalFlow: number
-): number {
-  const p_crit = inletPressure * criticalFlow;
-  return p_crit;
-}
-
-/**
- * Compute the mass flow rate through a sonic nozzle at sonic conditions
- * @param propertiesMethod - Method to compute properties
- * @param gasMixture - Gas mixture composition
- * @param inletPressure - Inlet pressure in kPa
- * @param outletPressure - Outlet pressure in kPa
- * @param temperature - Temperature in K
- * @param orificeDiameter - Orifice diameter in mm
- * @param orificeReynoldsNumber - Reynolds number for the orifice
- * @returns {number} - Mass flow rate in kg/s
- */
-function getMassFlowRateDataset(
-  propertiesMethod: Method,
-  gasMixture: GasMixture,
-  inletPressureRange: { min: number; max: number },
-  outletPressure: number,
-  temperature: number,
-  orificeDiameter: number,
-  orificeReynoldsNumber: number
-): MassFlowRate[] {
-  const output: MassFlowRate[] = [];
-  
-  const minPressure = inletPressureRange.min; //doubleSlider.value?.from || DoubleRange.props.defaultMin;
-  const maxPressure = inletPressureRange.max; //doubleSlider.value?.to || DoubleRange.props.defaultMax;
-  console.warn(`Pressure range: ${minPressure} to ${maxPressure}`);
-  if (!AGA8) {
-    console.warn("AGA8 module is not loaded");
-    throw new Error("AGA8 module is not loaded");
-  }
-
-  let properties: PropertiesDetailResult | PropertiesGERGResult;
-  let molarMass = 0; // Molar mass in g/mol
-  /** Discharge coefficient for thoroidal nozzle */
-  const Cd = getThoroidalNozzleDischargeCoefficient(orificeReynoldsNumber); // Discharge coefficient
-  /** Orifice area */
-  const A = Math.PI * (orificeDiameter / 1000 / 2) ** 2; // m^2
-  const SetupFunction =
-    propertiesMethod === "DETAIL" ? AGA8.SetupDetail : AGA8.SetupGERG;
-  const MolarMassFunction =
-    propertiesMethod === "DETAIL" ? AGA8.MolarMassDetail : AGA8.MolarMassGERG;
-  const PropertiesFunction =
-    propertiesMethod === "DETAIL" ? AGA8.PropertiesDetail : AGA8.PropertiesGERG;
-  SetupFunction();
-  molarMass = MolarMassFunction(gasMixture); // g/mol
-  const molarMassSI = molarMass / 1000; // kg/mol (SI units)
-  const { D: D_out } =
-    propertiesMethod === "DETAIL"
-      ? AGA8.DensityDetail(temperature, outletPressure, gasMixture)
-      : AGA8.DensityGERG(2, temperature, outletPressure, gasMixture); // mol/l
-  const rho_out = D_out * 1000 * molarMassSI; // kg/m³
-  const { D: D_1atm } =
-    propertiesMethod === "DETAIL"
-      ? AGA8.DensityDetail(temperature, 101.325, gasMixture)
-      : AGA8.DensityGERG(2, temperature, 101.325, gasMixture); // mol/l
-  const rho_1atm = D_1atm * 1000 * molarMassSI; // kg/m³
-  for (let i = 0; i < nbGraphSteps; i++) {
-    const P = minPressure + (i * (maxPressure - minPressure)) / nbGraphSteps;
-    const { D } =
-      propertiesMethod === "DETAIL"
-        ? AGA8.DensityDetail(temperature, P, gasMixture)
-        : AGA8.DensityGERG(2, temperature, P, gasMixture); // mol/l
-    properties = PropertiesFunction(temperature, D, gasMixture);
-    // Extract critical flow factor (Cf)
-    const Cf = properties.Cf;
-    /** Specific gas constant */
-    const Rs = R / molarMassSI; // J/(kg·K)
-    // Nozzle specific coefficient
-    const Kn = (Cd * Cf * (P * 1000)) / Math.sqrt(Rs * temperature);
-    // Calculate mass flow rate
-    // Q = Cd * Cf * A * P / sqrt(Rs * T)
-    // Q = Kn * A
-
-    const massFlow = getMaximalOutletPressure(P, Cf) < outletPressure ? NaN : Kn * A; // kg/s
-    output.push({ massFlowRate: massFlow,
-                  volumeFlowRateAtOutputPressure:massFlow/rho_out, 
-                  volumeFlowRateAt1atm:massFlow/rho_1atm,
-                  temperature, 
-                  pressure: P, 
-                  crticalPresure: getMaximalOutletPressure(P, Cf), 
-                  specificNozzleCoefficient: Kn, 
-                  kappa: properties.Kappa, 
-                  Cf: properties.Cf, 
-                  M: molarMassSI });
-    //Kn is used for computing the diameter knowing the mass flow rate
-    // A = Q / Kn
-    // ∏ * (D/2)^2 = Q / Kn
-    // D = sqrt(4 * Q / (Kn * ∏))
-  }
-  console.warn(
-    `Mass flow rate at mid range: ${output[
-      nbGraphSteps / 2
-    ].massFlowRate.toPrecision(4)} kg/s 
-    Volume flow rate at mid range: ${output[
-      nbGraphSteps / 2
-    ].volumeFlowRateAtOutputPressure.toPrecision(4)}m³/s Pressure: ${output[nbGraphSteps / 2].pressure
-    } kPa, Temperature: ${output[nbGraphSteps / 2].temperature} K
-      Critical pressure: ${output[nbGraphSteps / 2].crticalPresure} kPa`
-  );
-  // Polyfit
-  const x:number[] = [];
-  const y:number[] = [];
-  for (let i = 0; i < output.length; i++) {
-    if (!isNaN(output[i].massFlowRate)) {
-      x.push(output[i].pressure);
-      y.push(output[i].massFlowRate);
-    }
-  }
-  createPolynomialDisplay(output);
-  return output;
-}
-
-/**
- * Clean the dataset for removing NaN values
- * @param x the x values
- * @param y the f(x) values
- * @param maxDegree the maximum degree of the polynomial (optional default 100)
- * @param minCorrelation the minimum correlation coefficient (optional default 0.999)
- * @returns {terms: NumberArray, correlation: number} - Polynomial terms and correlation coefficient
- */
-function getPolyfitFlow(x: number[], y: number[],maxDegree?: number, minCorrelation?: number): {terms: NumberArray, correlation: number} {
-  const _x:number[] = [];
-  const _y:number[] = [];
-  if (maxDegree === undefined) {maxDegree = 100;}
-  if (minCorrelation === undefined) {minCorrelation = 0.999;}
-  if (x.length !== y.length) {
-    throw new Error("x and y arrays must have the same length");
-  }
-  else
-  {for (let i = 0; i < x.length; i++) {
-     if (!isNaN(y[i])) {
-       _x.push(x[i]);
-       _y.push(y[i]);
-     }
-   }
-   const _x64 = new Float64Array(_x as number[]);
-   const _y64 = new Float64Array(_y as number[]);
-   const polyfit = new Polyfit(_x64, _y64);
-   const terms = polyfit.computeBestFit(maxDegree,minCorrelation);
-   return {terms, correlation: polyfit.correlationCoefficient(terms)};}
-}
 
 /**
  * Get the LaTeX representation of a polynomial
@@ -991,15 +810,17 @@ function createChartLs(canvas: HTMLCanvasElement | null, data: MassFlowRate[]): 
           "
           class="border-e px-4 py-2 text-sm/none text-white hover:bg-teal-500 hover:text-white"
           @click="
-            createCharts(getMassFlowRateDataset(
+            getMassFlowRateDataset(
               method,
               getGasMixture(),
               { min: doubleSlider?.from as unknown as number, max: doubleSlider?.to as unknown as number },
               Pout,
               T,
               orificeDiameter,
-              THOROIDAL_RE_NUMBER
-            ))
+              THOROIDAL_RE_NUMBER,
+              AGA8,
+              nbGraphSteps
+            ).then((dataset)=>{createCharts(dataset);createPolynomialDisplay(dataset);})
           "
         >
           {{
@@ -1060,15 +881,17 @@ function createChartLs(canvas: HTMLCanvasElement | null, data: MassFlowRate[]): 
           "
           class="border-e px-4 py-2 text-sm/none text-white hover:bg-teal-500 hover:text-white"
           @click="
-            getExcel(getMassFlowRateDataset(
+            getMassFlowRateDataset(
               method,
               getGasMixture(),
               { min: doubleSlider?.from as unknown as number, max: doubleSlider?.to as unknown as number },
               Pout,
               T,
               orificeDiameter,
-              THOROIDAL_RE_NUMBER
-            ))
+              THOROIDAL_RE_NUMBER,
+              AGA8,
+              nbGraphSteps
+            ).then((dataset)=>{getExcel(dataset);createPolynomialDisplay(dataset);})
           "
         >
           {{
---
+++ src/aga8-sonic-nozzle-vue/src/utilities/sonic.ts
@@ -0,0 +1,211 @@
+/**
+ * @copyright Copyright (c) 2024-2025 Ronan LE MEILLAT
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import type { GasMixture, MainModule, PropertiesDetailResult, PropertiesGERGResult } from "@sctg/aga8-js";
+import AGA8wasm from "@sctg/aga8-js";
+import { Polyfit, type NumberArray } from "@sctg/polyfitjs";
+
+export const R = 8.31446261815324; // Universal gas constant in J/(mol·K)
+export type Method = "DETAIL" | "GERG-2008";
+export type MassFlowRate = {
+  massFlowRate: number; // mass flow rate in kg/s
+  volumeFlowRateAtOutputPressure: number; // volume flow rate in m³/s
+  volumeFlowRateAt1atm: number; // volume flow rate at 1 atm in m³/s
+  temperature: number; // temperature in K
+  pressure: number; // pressure in kPa
+  specificNozzleCoefficient: number; // specific nozzle coefficient
+  crticalPresure: number; // critical pressure in kPa
+  M: number; // Molar mass
+  kappa: number; // Isentropic exponent
+  Cf: number; //	Critical Flow coefficient
+};
+/**
+ * Compute the discharge coefficient for a toroidal nozzle given the Reynolds number
+ * cf: https://www.methyinfra.ptb.de/fileadmin/documents/empir-2021/methyinfra/documents/MetHyInfra_Nozzle_WS_MacDonald_Introduction_to_Sonic_Nozzles_and_ISO_9300.pdf p16
+ * @param Re_thoroidal - Reynolds number for thoroidal nozzle
+ */
+export function getThoroidalNozzleDischargeCoefficient(Re_thoroidal: number): number {
+    const Cd_a = 0.9959; // Constant for toroidal nozzle
+    const Cd_b = 2.72; // Reynolds number factor for toroidal nozzle
+    const Cd_n = 0.5; // Reynolds number exponent for toroidal nozzle
+    const Cd = Cd_a - Cd_b * Re_thoroidal ** (Cd_n * -1);
+    return Cd;
+  }
+  
+  /**
+   * Compute the maximal outlet pressure for a given inlet pressure and critical flow
+   * @param inletPressure
+   * @param criticalFlow
+   */
+  export function getMaximalOutletPressure(
+    inletPressure: number,
+    criticalFlow: number,
+  ): number {
+    const p_crit = inletPressure * criticalFlow;
+    return p_crit;
+  }
+
+  /**
+ * Compute the mass flow rate through a sonic nozzle at sonic conditions
+ * @param propertiesMethod - Method to compute properties
+ * @param gasMixture - Gas mixture composition
+ * @param inletPressure - Inlet pressure in kPa
+ * @param outletPressure - Outlet pressure in kPa
+ * @param temperature - Temperature in K
+ * @param orificeDiameter - Orifice diameter in mm
+ * @param orificeReynoldsNumber - Reynolds number for the orifice
+ * @param AGA8 - AGA8 module or undefined to load it
+ * @param datasetSteps - Number of elements for the dataset
+ * @returns {number} - Mass flow rate in kg/s
+ */
+export async function getMassFlowRateDataset(
+    propertiesMethod: Method,
+    gasMixture: GasMixture,
+    inletPressureRange: { min: number; max: number },
+    outletPressure: number,
+    temperature: number,
+    orificeDiameter: number,
+    orificeReynoldsNumber: number,
+    AGA8?: MainModule,
+    datasetSteps?: number
+  ): Promise<MassFlowRate[]> {
+    const output: MassFlowRate[] = [];
+    
+    const minPressure = inletPressureRange.min; //doubleSlider.value?.from || DoubleRange.props.defaultMin;
+    const maxPressure = inletPressureRange.max; //doubleSlider.value?.to || DoubleRange.props.defaultMax;
+    if (datasetSteps === undefined) {
+        datasetSteps = 1000;
+        }
+
+    if (AGA8 === undefined) {
+        AGA8 = await AGA8wasm();
+    }
+    console.warn(`Pressure range: ${minPressure} to ${maxPressure}`);
+    if (!AGA8) {
+      console.warn("AGA8 module is not loaded");
+      throw new Error("AGA8 module is not loaded");
+    }
+  
+    let properties: PropertiesDetailResult | PropertiesGERGResult;
+    let molarMass = 0; // Molar mass in g/mol
+    /** Discharge coefficient for thoroidal nozzle */
+    const Cd = getThoroidalNozzleDischargeCoefficient(orificeReynoldsNumber); // Discharge coefficient
+    /** Orifice area */
+    const A = Math.PI * (orificeDiameter / 1000 / 2) ** 2; // m^2
+    const SetupFunction =
+      propertiesMethod === "DETAIL" ? AGA8.SetupDetail : AGA8.SetupGERG;
+    const MolarMassFunction =
+      propertiesMethod === "DETAIL" ? AGA8.MolarMassDetail : AGA8.MolarMassGERG;
+    const PropertiesFunction =
+      propertiesMethod === "DETAIL" ? AGA8.PropertiesDetail : AGA8.PropertiesGERG;
+    SetupFunction();
+    molarMass = MolarMassFunction(gasMixture); // g/mol
+    const molarMassSI = molarMass / 1000; // kg/mol (SI units)
+    const { D: D_out } =
+      propertiesMethod === "DETAIL"
+        ? AGA8.DensityDetail(temperature, outletPressure, gasMixture)
+        : AGA8.DensityGERG(2, temperature, outletPressure, gasMixture); // mol/l
+    const rho_out = D_out * 1000 * molarMassSI; // kg/m³
+    const { D: D_1atm } =
+      propertiesMethod === "DETAIL"
+        ? AGA8.DensityDetail(temperature, 101.325, gasMixture)
+        : AGA8.DensityGERG(2, temperature, 101.325, gasMixture); // mol/l
+    const rho_1atm = D_1atm * 1000 * molarMassSI; // kg/m³
+    for (let i = 0; i < datasetSteps; i++) {
+      const P = minPressure + (i * (maxPressure - minPressure)) / datasetSteps;
+      const { D } =
+        propertiesMethod === "DETAIL"
+          ? AGA8.DensityDetail(temperature, P, gasMixture)
+          : AGA8.DensityGERG(2, temperature, P, gasMixture); // mol/l
+      properties = PropertiesFunction(temperature, D, gasMixture);
+      // Extract critical flow factor (Cf)
+      const Cf = properties.Cf;
+      /** Specific gas constant */
+      const Rs = R / molarMassSI; // J/(kg·K)
+      // Nozzle specific coefficient
+      const Kn = (Cd * Cf * (P * 1000)) / Math.sqrt(Rs * temperature);
+      // Calculate mass flow rate
+      // Q = Cd * Cf * A * P / sqrt(Rs * T)
+      // Q = Kn * A
+  
+      const massFlow = getMaximalOutletPressure(P, Cf) < outletPressure ? NaN : Kn * A; // kg/s
+      output.push({ massFlowRate: massFlow,
+                    volumeFlowRateAtOutputPressure:massFlow/rho_out, 
+                    volumeFlowRateAt1atm:massFlow/rho_1atm,
+                    temperature, 
+                    pressure: P, 
+                    crticalPresure: getMaximalOutletPressure(P, Cf), 
+                    specificNozzleCoefficient: Kn, 
+                    kappa: properties.Kappa, 
+                    Cf: properties.Cf, 
+                    M: molarMassSI });
+      //Kn is used for computing the diameter knowing the mass flow rate
+      // A = Q / Kn
+      // ∏ * (D/2)^2 = Q / Kn
+      // D = sqrt(4 * Q / (Kn * ∏))
+    }
+    console.warn(
+      `Mass flow rate at mid range: ${output[
+        datasetSteps / 2
+      ].massFlowRate.toPrecision(4)} kg/s 
+      Volume flow rate at mid range: ${output[
+        datasetSteps / 2
+      ].volumeFlowRateAtOutputPressure.toPrecision(4)}m³/s Pressure: ${output[datasetSteps / 2].pressure
+      } kPa, Temperature: ${output[datasetSteps / 2].temperature} K
+        Critical pressure: ${output[datasetSteps / 2].crticalPresure} kPa`
+    );
+    // Polyfit
+    const x:number[] = [];
+    const y:number[] = [];
+    for (let i = 0; i < output.length; i++) {
+      if (!isNaN(output[i].massFlowRate)) {
+        x.push(output[i].pressure);
+        y.push(output[i].massFlowRate);
+      }
+    }
+    return output;
+  }
+  
+/**
+ * Clean the dataset for removing NaN values and compute the polynomial fit
+ * @param x the x values
+ * @param y the f(x) values
+ * @param maxDegree the maximum degree of the polynomial (optional default 100)
+ * @param minCorrelation the minimum correlation coefficient (optional default 0.999)
+ * @returns {terms: NumberArray, correlation: number} - Polynomial terms and correlation coefficient
+ */
+export function getPolyfitFlow(x: number[], y: number[],maxDegree?: number, minCorrelation?: number): {terms: NumberArray, correlation: number} {
+    const _x:number[] = [];
+    const _y:number[] = [];
+    if (maxDegree === undefined) {maxDegree = 100;}
+    if (minCorrelation === undefined) {minCorrelation = 0.999;}
+    if (x.length !== y.length) {
+      throw new Error("x and y arrays must have the same length");
+    }
+    else
+    {for (let i = 0; i < x.length; i++) {
+       if (!isNaN(y[i])) {
+         _x.push(x[i]);
+         _y.push(y[i]);
+       }
+     }
+     const _x64 = new Float64Array(_x as number[]);
+     const _y64 = new Float64Array(_y as number[]);
+     const polyfit = new Polyfit(_x64, _y64);
+     const terms = polyfit.computeBestFit(maxDegree,minCorrelation);
+     return {terms, correlation: polyfit.correlationCoefficient(terms)};}
+  }
