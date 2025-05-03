70315671a94e9a2cd1df26b68cfc72f7f149798e
æLtorio <aeltorio@universidadatlantico.org>
2025-05-03 14:38:36 +0200

    chore: update dependencies and add GitHub Pages SPA plugin
    - Updated chart.js from ^4.4.8 to ^4.4.9
    - Updated temml from ^0.11.1 to ^0.11.6
    - Updated @eslint/js from ^9.21.0 to ^9.26.0
    - Added @sctg/vite-plugin-github-pages-spa as a dependency
    - Updated @tailwindcss/postcss from ^4.0.9 to ^4.1.5
    - Updated @typescript-eslint/eslint-plugin and parser from ^8.25.0 to ^8.31.1
    - Updated @vitejs/plugin-vue from ^5.2.1 to ^5.2.3
    - Updated autoprefixer from ^10.4.20 to ^10.4.21
    - Updated eslint from ^9.21.0 to ^9.26.0
    - Updated sass-embedded from ^1.85.1 to ^1.87.0
    - Updated tailwindcss from ^4.0.9 to ^4.1.5
    - Updated typescript from ~5.7.3 to ~5.8.3
    - Updated typescript-eslint from ^8.25.0 to ^8.31.1
    - Updated vite from ^6.2.0 to ^6.3.4
    - Updated vue-tsc from ^2.2.4 to ^2.2.10
    - Added githubPagesSpa plugin to Vite configuration

--- src/aga8-sonic-nozzle-vue/vite.config.ts
+++ src/aga8-sonic-nozzle-vue/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), githubPagesSpa()],
   optimizeDeps: {
     exclude: ['@sctg/aga8-js']
   },
